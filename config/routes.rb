b6da50aa566471b4023ca4a94c83161a9a55ad2e
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-06-26 18:33:20 +0000

    [Finish]RSpecテスト終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+  get "/top" => "homes#top"
+  root to: "homes#top"
+
   resources :books
+
 end
+
+
+  #     Prefix Verb   URI Pattern                         Controller#Action
+  #       top GET     /top(.:format)                      homes#top
+  #       root GET    /                                   homes#top
+  #     books GET     /books(.:format)                    books#index
+  #           POST    /books(.:format)                    books#create
+  #   new_book GET    /books/new(.:format)                books#new
+  # edit_book GET     /books/:id/edit(.:format)           books#edit
+  #       book GET    /books/:id(.:format)                books#show
+  #           PATCH   /books/:id(.:format)                books#update
+  #           PUT     /books/:id(.:format)                books#update
+  #           DELETE  /books/:id(.:format)                books#destroy
